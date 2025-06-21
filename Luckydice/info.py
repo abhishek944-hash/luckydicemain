b227cbe3eddad4ccd33f3df7b87ae9857cf7fd86
Abhishek <abhishektharkar2001@gmail.com>
2025-06-22 00:53:15 +0530

    Refactor: moved email credentials to .env using python-decouple

--- Luckydice/info.py
+++ Luckydice/info.py
@@ -1,5 +1,7 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "Qualdedigital.for.add2012@gmail.com"
-EMAIL_HOST_PASSWORD = "ckrb xvji piri cddg"
-EMAIL_PORT = 587
+from decouple import config
+
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
